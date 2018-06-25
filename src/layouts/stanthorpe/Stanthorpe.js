c91eb99737f18fec417ad010366a2a8eed8ba655
Thomas Miller <thomas.miller@consensys.net>
2018-06-25 20:45:55 +1000

    fix div error

--- src/layouts/stanthorpe/Stanthorpe.js
+++ src/layouts/stanthorpe/Stanthorpe.js
@@ -5,27 +5,37 @@ import { Image } from 'semantic-ui-react'
 
 
 const Stanthorpe = () => (
-        <main className="container">
-            <div className="pure-g">
-                <div className="pure-u-1-1 header">
-                    <h1>Stanthrope Network Plans</h1>
-                    <p>Here are the current Network plans</p>
-                    <Image src='https://content.screencast.com/users/tommycp2/folders/Snagit/media/2ec2c7dc-069e-4acd-a06d-ba6ef20b972d/2018-06-25_20-28-44.png' fluid />
+    <main className="container">
+        <div className="pure-g">
+            <div className="pure-u-1-1 header">
+                <h1>Stanthrope Network Plans</h1>
+                <p>Here are the current Network plans</p>
+                <Image src='https://content.screencast.com/users/tommycp2/folders/Snagit/media/2ec2c7dc-069e-4acd-a06d-ba6ef20b972d/2018-06-25_20-28-44.png' fluid />
 
-                    <br /><br />
-                </div>
-
-                <LoadingContainer>
-                    <div className="pure-u-1-1">
-                        <h2>Current Ether</h2>
-                        <AccountData accountIndex="0" units="ether" precision="3" />
-
-                        <br /><br />
-                    </div>
-                </LoadingContainer>
-
+                <br /><br />
+                <h4>Community Connection Fee: $20,000 AUD (payable in dCom Tokens.</h4>
+                <br />
             </div>
-        </main>
+
+        </div>
+
+        <div>
+            <p>asdfasd</p>
+        </div>
+
+        <LoadingContainer>
+            <div className="pure-u-1-1">
+                <h2>Current Ether</h2>
+                <AccountData accountIndex="0" units="ether" precision="3" />
+
+                <br /><br />
+            </div>
+        </LoadingContainer>
+
+        <div>
+            <p>asdfasd</p>
+        </div>
+    </main>
 )
 
 export default Stanthorpe
