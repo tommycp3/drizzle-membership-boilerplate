7301277304dcc7807973cdafa24ce9c09d5287eb
Thomas Miller <thomas.miller@consensys.net>
2018-06-27 15:47:45 +1000

    fix erro on dashboard page

--- src/layouts/dashboard/Dashboard.js
+++ src/layouts/dashboard/Dashboard.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react'
+import { Header, Accordion, Icon, List, Message, Button } from 'semantic-ui-react'
 
-class Dashboard extends Component {
-  constructor(props, { authData }) {
-    super(props)
-    authData = this.props
+export default class Dashboard extends Component {
+  state = { activeIndex: 0 }
+
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps
+    const { activeIndex } = this.state
+    const newIndex = activeIndex === index ? -1 : index
+
+    this.setState({ activeIndex: newIndex })
   }
 
   render() {
-    return(
-      <main className="container">
-        <div className="pure-g">
-          <div className="pure-u-1-1">
-            <h1>Dashboard</h1>
-            <p><strong>Congratulations {this.props.authData.name}!</strong> If you're seeing this page, you've logged in with your own smart contract successfully.</p>
-          </div>
-        </div>
-      </main>
+    const { activeIndex } = this.state
+
+    return (
+
+      <div className="container" >
+
+        <Header size='huge'>Congratulations on Executing your First Smart Contract</Header>
+
+        <p>Now that you have made your first purchase into the AOC system we want to teach you how to explore the blockchain, and trace where the ETH you just sent went.</p>
+        <br />
+
+        <h4>Why do we want to do this?</h4>
+        <p>We want to empower you with the new tools blockchain technology offers so that you can see how this business operates and make your own judgement on how this system operates.</p>
+        <p>We believe all businesses, charities, governments should operate with this technology, as we think it’s the best way for people to make their own judgement and work out if they want to participate or not.</p>
+
+
+
+
+
+
+
+      </div >
     )
   }
 }
-
-export default Dashboard
